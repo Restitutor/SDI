9032c8105b58d5a57bdfa55c378dc0ec3f67e948
Restitutor <nissar@mortemale.org>
2026-01-11 23:28:59 +0000

    Update Paperweight plugin and version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     `java-library`
-    id("io.papermc.paperweight.userdev") version "2.0.0-beta.17"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
 }
 
 group = "me.arcator"
 
-version = "3.0"
+version = "3.1"
 
 description = "Assorted entity utils."
 
@@ -17,7 +17,7 @@ repositories {
 java { toolchain.languageVersion.set(JavaLanguageVersion.of(21)) }
 
 dependencies {
-    paperweight.paperDevBundle("1.21.5-R0.1-SNAPSHOT")
+    paperweight.paperDevBundle("1.21.11-R0.1-SNAPSHOT")
 }
 
 tasks {
